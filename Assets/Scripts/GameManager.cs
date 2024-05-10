87cd07e94b8580b493c13160b638bbc82e31d81a
Ian W <waskomian@gmail.com>
2024-05-09 23:09:50 -0500

    Made Reset Counter persist between levels

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static GameManager Instance;
+    public int ResetCount {get; private set; }
+    void Awake(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncrementResetCounter(){
+        ResetCount++;
+    }
+}
